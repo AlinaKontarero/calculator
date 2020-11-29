6336a7577d0ba620f6ae954f75b79027081c13cc
alina-kontarero <alina.kontarero@gmail.com>
2020-11-30 09:54:08 +1100

    Column (bulma) removed from ResultPanel

--- src/views/ResultPanel.tsx
+++ src/views/ResultPanel.tsx
@@ -6,10 +6,11 @@ interface Props {
 }
 
 const ResultPanel= (props: Props) =>  {
+  console.log('result:::: ', props.result)
   const numericalResult = parseInt(props.result)
   const res = Number.isInteger(numericalResult) ? formatThousands(numericalResult) : props.result;
   return (
-    <div className='column is-full'>
+    <div className='result-panel'>
       <p className='result'> {res} </p> 
     </div>
   );
