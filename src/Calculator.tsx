6bc882fa9cd109bc1a5fa9389c0041ba6b0a974e
alina-kontarero <alina.kontarero@gmail.com>
2020-11-30 19:00:41 +1100

    Updated fns for calculator

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -6,73 +6,139 @@ import ScientificPanel from './views/ScientificPanel'
 import theme from './utils/theme'
 import './styles/global-styles.css'
 
+type IOperator = '+' | '-' | '*' | '/' | '='
+
 interface State {
-  result: string
+  displayedResult: string,
+  isOperatorNext: boolean
+  calculatedResult: number | null,
+  operator: null | IOperator,
 }
 
 class Calculator extends React.Component<{}, State> {
   constructor(props: any) {
     super(props)
-    this.state = { result: '' }
-  }
-
-  private removeChar = () => this.setState({
-    result: this.state.result.slice(0, -1)
-  })
-
-  private revertSign = () => {
-    const oppositeValue: number = parseInt(this.state.result) * (-1);
-    this.setState({ result: oppositeValue.toString() })
-  }
-
-  private calculate = () => {
-    try {
-        this.setState({
-          // eslint-disable-next-line
-          result: (eval(this.state.result) || "" ) + ""
-        })
-    } catch (e) {
-        this.setState({
-            result: "error"
-        })
+    this.state = { 
+      displayedResult: '0',
+      isOperatorNext: false,
+      calculatedResult: null,
+      operator: null
     }
-};
-
-private addValue = (_value: string) => {
-  // const numRegex = /^[0-9]*$/;
-  // // const isNumericalValue: boolean = !!_value.match(numRegex)
-  // const zeroAtBeginning: boolean = this.state.result.length === 1 && 
-  //   this.state.result[0] === '0' // && isNumericalValue
-
-  // if(zeroAtBeginning) {
-    // this.setState({ result: _value })
-    // } else {
-      this.setState({
-        result: this.state.result + _value
-    })
-  // }
-}
-
-private onClick = (_buttonValue: string) => {
-  if(_buttonValue === 'C') {
-    this.removeChar()
-  } else if(_buttonValue === '=') {
-    this.calculate()
-  } else if(_buttonValue === '+/-') {
-    this.revertSign()
-  } else {
-    this.addValue(_buttonValue)
   }
-}
+ 
+  private setDecimals = () => {    
+    if (!(/\./).test(this.state.displayedResult)) {
+      this.setState({
+        displayedResult: this.state.displayedResult + '.',
+        isOperatorNext: false
+      })
+    }
+  }
+
+  private removeLastChar = () => {
+    this.setState({
+      displayedResult: this.state.displayedResult.substring(0, this.state.displayedResult.length - 1) || '0'
+    })
+  }
+  
+  private setOppositeSign = () =>  {
+    const newValue = parseFloat(this.state.displayedResult) * (-1)
+    this.setState({
+      displayedResult: String(newValue)
+    })
+  }
+  
+ private getPercentage = () => {
+    const currentValue = parseFloat(this.state.displayedResult)
+    if (currentValue === 0) {
+      return
+    }
+    
+    const fixedDigits = this.state.displayedResult.replace(/^-?\d*\.?/, '')
+    const res = parseFloat(this.state.displayedResult) / 100
+    
+    this.setState({
+      displayedResult: String(res.toFixed(fixedDigits.length + 2))
+    })
+  }
+    
+  private handleIntegers = (int: number) => {
+    const { displayedResult, isOperatorNext } = this.state
+    if (isOperatorNext) {
+      this.setState({
+        displayedResult: String(int),
+        isOperatorNext: false
+      })
+    } else {
+      this.setState({
+        displayedResult: displayedResult === '0' ? String(int) : displayedResult + int
+      })
+    }
+  }
+  
+  private performOperation = (nextOperator: IOperator) => {    
+    const { calculatedResult, displayedResult, operator } = this.state
+    const inputValue = parseFloat(displayedResult)
+    
+    if (calculatedResult === null) {
+      this.setState({
+        calculatedResult: inputValue
+      })
+    } else if (operator) {
+      const currentValue = calculatedResult || 0
+      const newValue = this.handleMath(operator, currentValue, inputValue)
+      
+      this.setState({
+        calculatedResult: newValue,
+        displayedResult: String(newValue)
+      })
+    }
+    
+    this.setState({
+      isOperatorNext: true,
+      operator: nextOperator
+    })
+  }
+
+  private handleMath = (operator: IOperator, current: number, addedValue: number ) => {
+    if(operator === '+') {
+      return current + addedValue
+    } else if(operator === '-') {
+      return current - addedValue
+    } else if(operator === '*') {
+      return current * addedValue
+    } else if(operator === '/') {
+      return current / addedValue
+    } else return addedValue
+  }
+  
+  private handleCalculation = (_value: IOperator | string) => {
+    const isOperator: boolean = _value === '+' || _value === '-' || _value === '/' || _value === '*' || _value === '='
+
+    if ((/\d/).test(_value)) {
+      this.handleIntegers(parseInt(_value, 10))
+    } else if (isOperator) {
+      this.performOperation(_value as IOperator)
+    } else if (_value === '.') {
+      this.setDecimals()
+    } else if (_value === '%') {
+      this.getPercentage()
+    } else if (_value === 'C') {
+      this.removeLastChar()
+    } else if(_value === '+/-') {
+      this.setOppositeSign()
+    }
+  };
+
 
   public render() {
     return (
       <ThemeProvider theme={theme}>
         <div className='grid-container panel'> 
-          <ResultPanel result={this.state.result} />
+          <ResultPanel result={this.state.displayedResult} />
           <div className='grid-container calculator'>
             <ScientificPanel />
-            <ButtonsPanel onClick={this.onClick} />
+            <ButtonsPanel onClick={this.handleCalculation} />
         </div> 
         </div>
       </ThemeProvider>
